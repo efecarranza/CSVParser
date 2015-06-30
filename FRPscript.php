a19bb66f99b93f7e0ec147782d0567203e353736
Fermin Carranza <fcarranza@watscoventures.com>
2015-06-30 10:17:14 -0400

    First commit

---
+++ FRPscript.php
@@ -0,0 +1,54 @@
+<?php
+
+  $frpFile = 'FRPUNVA1.txt';
+  $outputFile = 'flat_price_master.csv';
+
+  $fin = fopen($frpFile, 'r');
+  $fout = fopen($outputFile, 'w');
+
+   $categories = array(
+      'Air Flow' => array('ACA', 'VACA', 'PAPA'),
+      'Electrical' => array('APP', 'BAA', 'BABA'),
+    );
+
+  $headers = array(
+          'Item Code',
+          'Main Category',
+          'Sub-Category',
+          'Item Name',
+          'Description (optional)',
+          'Retail Price Standard',
+          'Retail Price Overtime)',
+      );
+  fputcsv($fout, $headers);
+
+  while (!feof($fin)) {
+    $linesToCsv = array();
+    $line = fgets($fin);
+    $lineSanitized = explode("\t", $line);
+
+    array_push($linesToCsv, $lineSanitized[1]);
+    $mainCategory = DetermineCategory($lineSanitized[0], $categories);
+    array_push($linesToCsv, $mainCategory);
+    array_push($linesToCsv, $lineSanitized[0]);
+    array_push($linesToCsv, $lineSanitized[2]);
+    array_push($linesToCsv, $lineSanitized[3]);
+    array_push($linesToCsv, $lineSanitized[15]);
+    array_push($linesToCsv, $lineSanitized[20]);
+
+    fputcsv($fout, $linesToCsv);
+
+  }
+
+  fclose($fin);
+  fclose($fout);
+
+  function DetermineCategory($subcategory, $categories) {
+    foreach($categories as $categoryKey => $category) {
+      if (in_array($subcategory, $category)) {
+        return $categoryKey;
+      }
+    }
+  }
+
+?>
